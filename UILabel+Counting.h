23c84ab85f1a5ef94a9c6e57a7618a1163996bbd
Tim Gostony <tim@scopely.com>
2013-02-08 09:22:31 -0800

    Added enum type for counting method

--- UILabel+Counting.h
+++ UILabel+Counting.h
@@ -1,6 +1,15 @@
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    UILabelCountingMethodLinear,
+    UILabelCountingMethodEaseIn,
+    UILabelCountingMethodEaseOut,
+    UILabelCountingMethodEaseInOut
+} UILabelCountingMethod;
+
 @interface UILabel (Counting)
+
+@property UILabelCountingMethod countingMethod;
 
 @end
 
