2eccab2b3bcc094c5d3d177dad065aa135d1fd05
Tim Gostony <tim@scopely.com>
2013-02-08 10:40:24 -0800

    Added more examples.

--- CountingTestProject/CTPViewController.m
+++ CountingTestProject/CTPViewController.m
@@ -21,11 +21,18 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    
+    // make one that counts up
     UICountingLabel* myLabel = [[UICountingLabel alloc] initWithFrame:CGRectMake(10, 10, 100, 40)];
     [self.view addSubview:myLabel];
-    [myLabel setValue:100 withCountingMethod:UILabelCountingMethodLinear andDuration:5.0];
+    [myLabel setValue:100 withCountingMethod:UILabelCountingMethodLinear andDuration:2.0];
     [myLabel release];
+    
+    // make one that counts down
+    UICountingLabel* countingDownLabel = [[UICountingLabel alloc] initWithFrame:CGRectMake(10, 50, 100, 40)];
+    [self.view addSubview:countingDownLabel];
+    countingDownLabel.text = @"200";
+    [countingDownLabel setValue:100 withCountingMethod:UILabelCountingMethodLinear andDuration:2.0];
+    [countingDownLabel release];
     
 }
 
