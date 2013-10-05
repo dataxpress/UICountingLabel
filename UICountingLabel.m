143966bf52c4d8633104bdc78831b2464cb3c9ae
Tim Gostony <tim@scopely.com>
2013-10-05 02:21:37 -0700

    Added warning about requiring ARC.

--- UICountingLabel.m
+++ UICountingLabel.m
@@ -1,5 +1,8 @@
 #import "UICountingLabel.h"
 
+#if !__has_feature(objc_arc)
+#error UICountingLabel is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
+#endif
 
 #pragma mark - UILabelCounter
 
