b0e27cd448c7bd502bde61062b9d24e83970ead3
Sergey Kovalev <44sergey@gmail.com>
2015-02-11 21:35:02 -0800

    remove header info

--- UICountingLabel.swift
+++ UICountingLabel.swift
@@ -1,12 +1,3 @@
-//
-//  SKCountingLabel.swift
-//  bellogo
-//
-//  Created by Sergey Kovalev on 2/11/15.
-//  Copyright (c) 2015 Sergey Kovalev. All rights reserved.
-//
-
-import Foundation
 import UIKit
 
 enum UILabelCountingMethod: Int{
