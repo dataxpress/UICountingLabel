d3f89e703efadf87a261bd8235d184c51514ba8c
CWalig <c.walig@gmail.com>
2015-03-31 16:33:07 +1300

    Update UICountingLabel.m

--- UICountingLabel.m
+++ UICountingLabel.m
@@ -208,11 +208,19 @@
         // check if counting with ints - cast to int
         if([self.format rangeOfString:@"%(.*)d" options:NSRegularExpressionSearch].location != NSNotFound || [self.format rangeOfString:@"%(.*)i"].location != NSNotFound )
         {
-            self.text = [NSString stringWithFormat:self.format,(int)value];
+            if (self.shouldBeLocalized) {
+                self.text = [NSString localizedStringWithFormat:self.format,(int)value];
+            } else {
+                self.text = [NSString stringWithFormat:self.format,(int)value];
+            }
         }
         else
         {
-            self.text = [NSString stringWithFormat:self.format,value];
+            if (self.shouldBeLocalized) {
+                self.text = [NSString localizedStringWithFormat:self.format,value];
+            } else {
+                self.text = [NSString stringWithFormat:self.format,value];
+            }
         }
     }
 }
