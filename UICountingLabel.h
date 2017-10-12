847397244fd32f657e7e16ec31b49fc4643d2bbf
Juan Navas <juannavas7@gmail.com>
2017-10-12 11:13:53 +0200

    fix: removing a warning in a block declaration feat: updated deployment target to iOS 7.0 (minimum to build with Xcode 9) feat: updated podspec's deployment target to iOS 7.0 (as Xcode's project)

--- UICountingLabel.h
+++ UICountingLabel.h
@@ -19,7 +19,7 @@ typedef NSAttributedString* (^UICountingLabelAttributedFormatBlock)(CGFloat valu
 
 @property (nonatomic, copy) UICountingLabelFormatBlock formatBlock;
 @property (nonatomic, copy) UICountingLabelAttributedFormatBlock attributedFormatBlock;
-@property (nonatomic, copy) void (^completionBlock)();
+@property (nonatomic, copy) void (^completionBlock)(void);
 
 -(void)countFrom:(CGFloat)startValue to:(CGFloat)endValue;
 -(void)countFrom:(CGFloat)startValue to:(CGFloat)endValue withDuration:(NSTimeInterval)duration;
